983441b7fe01d1c78c5b837348d842a57e75ce56
jmslezak01 <jmslezak01@gmail.com>
2024-06-18 08:55:07 -0500

    Hello World App with Curl error 60 Self Cert

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'Enter the clietID from Developer Portal',
-    'client_secret' => 'Enter the clientSecret from Developer Portal',
+    'client_id' => 'AB0R3tIuBaH20sAzrNIf6mXkcsMfEoouD1Sat37XYGcBAmIKA7',
+    'client_secret' => 'qYrPtcXKvlzNmkI3VLUBEQPcUcURDmA4XxE9sAdl',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
 )
